66359e0c0555873d286391d8202ca1ba0b71fe8d
Veer Pratap Singh <xyresiiic@gmail.com>
2026-08-16 18:25:10 +0530

    Update updateRecord.java

--- Beta1_CSE_JdbcConnection/src/com/jdbc/updateRecord.java
+++ Beta1_CSE_JdbcConnection/src/com/jdbc/updateRecord.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class updateRecord {
     public static void main(String[] args)throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/beta1cse","root","Vedhi.0931");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/beta1cse","root","pass");
 
         Statement statement = con.createStatement();
         statement.executeUpdate("update student set name = 'Bender' where id=101");
