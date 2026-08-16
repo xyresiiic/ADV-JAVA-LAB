63b7ee24ba5d5edfa7258bf61e41dc2bee02367e
Veer Pratap Singh <xyresiiic@gmail.com>
2026-08-16 18:24:10 +0530

    Update DeleteRecord.java

--- Beta1_CSE_JdbcConnection/src/com/jdbc/DeleteRecord.java
+++ Beta1_CSE_JdbcConnection/src/com/jdbc/DeleteRecord.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class DeleteRecord {
     public static void main(String[] args)throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/beta1cse","root","Vedhi.0931");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/beta1cse","root","pass");
 
         Statement statement = con.createStatement();
         statement.executeUpdate("delete from student where id=101");
