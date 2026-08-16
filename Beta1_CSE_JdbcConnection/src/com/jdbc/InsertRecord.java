279e6bde2fe9f778a2297bd218415c69f6715a2a
Veer Pratap Singh <xyresiiic@gmail.com>
2026-08-16 18:24:33 +0530

    Update InsertRecord.java

--- Beta1_CSE_JdbcConnection/src/com/jdbc/InsertRecord.java
+++ Beta1_CSE_JdbcConnection/src/com/jdbc/InsertRecord.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class InsertRecord {
     public static void main(String[] args)throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/beta1cse","root","Vedhi.0931");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/beta1cse","root","pass");
 
         Statement statement = con.createStatement();
         statement.executeUpdate("insert into student values(105,'Nidhi')");
